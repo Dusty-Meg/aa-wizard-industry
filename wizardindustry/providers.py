cd70670cafe7c1f3d9a0a27df240dc6ed140df91
Matthew Moore <matthewemmoore@googlemail.com>
2025-12-27 18:26:41 +0000

    14 (#15)
    * Add django-esi dependency and update EsiClientProvider import
    
    * Fix import casing for ESIClientProvider
    
    * Update dependencies in pyproject.toml and adjust app_name formatting in urls.py
    
    * Remove unused import of os in providers.py

--- wizardindustry/providers.py
+++ wizardindustry/providers.py
@@ -1,17 +1,21 @@
-# Standard Library
-import os
-
 # Alliance Auth
-from esi.clients import EsiClientProvider
+from esi.openapi_clients import ESIClientProvider
 
 from . import __version__
 
-
-def get_swagger_spec_path() -> str:
-    """returns the path to the current swagger spec file"""
-    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "swagger.json")
-
-
-esi = EsiClientProvider(
-    app_info_text=f"aa-wizard-industry v{__version__}",
+esi = ESIClientProvider(
+    compatibility_date="2025-12-16",
+    ua_appname="aa-wizard-industry",
+    ua_version=__version__,
+    operations=[
+        "GetCharactersCharacterIdRoles",
+        "GetUniverseStationsStationId",
+        "GetUniverseStructuresStructureId",
+        "GetCharactersCharacterIdIndustryJobs",
+        "GetCorporationsCorporationIdIndustryJobs",
+        "GetCharactersCharacterIdAssets",
+        "GetCorporationsCorporationIdAssets",
+        "PostCorporationsCorporationIdAssetsNames",
+        "PostCharactersCharacterIdAssetsNames",
+    ],
 )
